d7cfb01c304756937fee20fd661a63669829705e
Joseph McKee <jsphmck92@gmail.com>
2022-06-06 23:04:26 +1000

    No enter pressing required

--- blob_support/frontend/test_blob_transmit.c
+++ blob_support/frontend/test_blob_transmit.c
@@ -3,23 +3,40 @@
 #include <math.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <termios.h>            //termios, TCSANOW, ECHO, ICANON
+#include <unistd.h> 
 
 int
 main(int argc, char **argv)
 
 {
+    static struct termios oldt, newt;
+    char c;
+    /*tcgetattr gets the parameters of the current terminal
+    STDIN_FILENO will tell tcgetattr that it should write the settings
+    of stdin to oldt*/
+    tcgetattr( STDIN_FILENO, &oldt);
+    /*now the settings will be copied*/
+    newt = oldt;
+
+    /*ICANON normally takes care that one line at a time will be processed
+    that means it will return if it sees a "\n" or an EOF or an EOL*/
+    newt.c_lflag &= ~(ICANON);          
+
+    /*Those new settings will be set to STDIN
+    TCSANOW tells tcsetattr to change attributes immediately. */
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+
     BLOB_SOCKET_INIT("192.168.50.115", 8000);
     
-    while (1)
+    while (c = getchar())
     {
         int forward = 0;
         int backward = 0;
         int left = 0;
         int right = 0;
         int stop = 0;
-        char c;
-
-        c = getchar();
+        
 
         if (c == 'w')
         {
